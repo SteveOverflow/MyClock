c5a4a7455afa4263df41cf69649aa8d26b7c4d4c
Stephen Tanton <stephen.tanton@gmail.com>
2015-06-12 23:10:47 -0300

    adds bluetooth support, code clean up
    Adds functionality to make use of HC-05 bluetooth module as well as
    icons for notification dislpay.  Also code clean up.

--- MyClock.ino
+++ MyClock.ino
@@ -1,3 +1,5 @@
+#include <SoftwareSerial.h>
+
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -9,44 +11,117 @@ Adafruit_SSD1306 display(OLED_RESET);
 
 #define DS3231_I2C_ADDRESS 0x68
 
-unsigned long lastToggle;
-boolean showColon = true;
+const int btConnectPin = 12;
+
+SoftwareSerial bluetooth(10, 11);
+
+// bitmaps for display of notifications
+#define LOGO16_GLCD_HEIGHT 16 
+#define LOGO16_GLCD_WIDTH  16 
+
+//facebook notification icon
+static const unsigned char PROGMEM fb_bmp[] ={ 
+  B00001111, B11110000,
+  B00111111, B11111100,
+  B01111111, B11111110,
+  B01111111, B11111110,
+  B11111111, B11000011,
+  B11111111, B10000011,
+  B11111111, B10001111,
+  B11111111, B10011111,
+  B11111110, B00000011,
+  B11111110, B00000111,
+  B11111111, B10011111,
+  B11111111, B10011111,
+  B01111111, B10011110,
+  B01111111, B10011110,
+  B00111111, B10011100,
+  B00001111, B10010000 };
+  
+//exclamation mark icon  
+static const unsigned char PROGMEM exclamation_bmp[] = {
+  B00001111, B11110000,
+  B00111111, B11111100,
+  B01111110, B01111110,
+  B01111100, B00111110,
+  B11111000, B00011111,
+  B11111000, B00011111,
+  B11111000, B00011111,
+  B11111000, B00011111,
+  B11111100, B00111111,
+  B11111100, B00111111,
+  B11111110, B01111111,
+  B11111111, B11111111,
+  B01111110, B01111110,
+  B01111110, B01111110,
+  B00111111, B11111100,
+  B00001111, B11110000  
+};
+
+//bluetooth connection icon
+static const unsigned char PROGMEM bluetooth_bmp[] ={
+   B00000000, B00000000,
+   B00000000, B00000000,
+   B00000001, B11000000,
+   B00000001, B11100000,
+   B00000001, B10110000,
+   B00011001, B10011000,
+   B00001101, B10110000,
+   B00000111, B11100000,
+   B00000001, B10000000,
+   B00000111, B11100000,
+   B00001101, B10110000,
+   B00011001, B10011000,
+   B00000001, B10110000,
+   B00000001, B11100000,
+   B00000001, B11000000,
+   B00000000, B00000000
+};
+
+//hangouts notificaiton icon
+static const unsigned char PROGMEM hangouts_bmp[] = {
+  B00000000, B00000000,//1
+  B00000111, B11100000,//2
+  B00001111, B11110000,//3
+  B00011111, B11111000,//4
+  B00111111, B11111100,//5
+  B01111111, B11111110,//6
+  B01110001, B10001110,//7
+  B01110001, B10001110,//8
+  B01110001, B10001110,//9
+  B01111101, B11101110,//10
+  B01111011, B11011110,//11
+  B00111111, B11111100,//12
+  B00011111, B11111000,//13
+  B00001111, B11110000,//14
+  B00000111, B11100000,//15
+  B00000000, B00000000//16
+};
 
 void setup() {
   Serial.begin(9600);
+  bluetooth.begin(9600);
   Wire.begin();
   display.begin(SSD1306_SWITCHCAPVCC, OLED_I2C_ADDRESS);
-  lastToggle = millis();
+  
+  pinMode(btConnectPin, INPUT);
 }
+
+boolean showHangouts = false;
+boolean showFacebook = false;
+boolean showExclamation = false;
 
 void loop() {
   display.display();
   display.clearDisplay();
-  //delay(2000);
-  boolean showColon = true;
-  lastToggle = millis();
   
   byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;
-  
   readTime(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
-  
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.print(formatDayOfWeek(dayOfWeek));
-  display.print(" ");
-  display.print(formatMonth(month));
-  display.print(" ");
-  display.print(formatNumber(dayOfMonth));
-  display.setTextSize(1);
-  display.println();
-  display.setTextSize(2);
-  display.print(" ");
-  display.print(formatNumber(hour));
-  display.print(":");
-  display.print(formatNumber(minute));
-  display.print(".");
-  display.print(formatNumber(second));
+  displayDateTime(second, minute, hour, dayOfWeek, dayOfMonth, month, year);
+ 
+  showBluetooth();
+  setIcons();
+  showIcons();
  
   display.display();
 }
@@ -74,6 +149,94 @@ void readTime(byte *second,
   *dayOfMonth = bcdToDec(Wire.read());
   *month = bcdToDec(Wire.read());
   *year = bcdToDec(Wire.read());  
+}
+
+void displayDateTime(byte second,
+              byte minute,
+              byte hour,
+              byte dayOfWeek,
+              byte dayOfMonth,
+              byte month,
+              byte year){
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.print(formatDayOfWeek(dayOfWeek));
+  display.print(" ");
+  display.print(formatMonth(month));
+  display.print(" ");
+  display.print(formatNumber(dayOfMonth));
+  display.setTextSize(1);
+  display.println();
+  display.setTextSize(2);
+  display.print("  ");
+  display.print(formatNumber(hour));
+  display.print(":");
+  display.print(formatNumber(minute));
+  
+  display.setTextSize(1);
+  display.print(" ");
+  display.print(formatNumber(second));                  
+}
+
+void showBluetooth(){
+  if(digitalRead(btConnectPin)==HIGH){
+    display.drawBitmap(0, 48, bluetooth_bmp, 16, 16, WHITE);
+  }
+}
+
+void setIcons(){
+  while(bluetooth.available()){
+    char op = bluetooth.read();
+    if(op=='+'||op=='-'){
+       char app = bluetooth.read();
+       setIconFlags(op, app);
+    }
+  }
+}
+
+void setIconFlags(char op, char app){
+  if(app=='h'){
+    if(op=='+'){
+      showHangouts = true;
+    }
+    if(op=='-'){
+      showHangouts = false; 
+    }
+  }
+  
+  if(app=='f'){
+    if(op=='+'){
+      showFacebook = true;
+    }
+    if(op=='-'){
+      showFacebook = false;
+    }
+  }
+  
+  if(app=='o'){
+    if(op=='+'){
+
+      showExclamation = true;
+    }
+    if(op=='-'){
+      showExclamation = false;
+    }
+  }
+}
+
+void showIcons(){
+  if(showHangouts){
+    display.drawBitmap(18, 48, hangouts_bmp, 16, 16, WHITE); 
+  }
+  
+  if(showFacebook){
+    display.drawBitmap(36, 48, fb_bmp, 16, 16, WHITE);
+  }
+  
+  if(showExclamation){
+    display.drawBitmap(54, 48, exclamation_bmp, 16, 16, WHITE); 
+  }
 }
 
 String formatDayOfWeek(byte dayOfWeek){
